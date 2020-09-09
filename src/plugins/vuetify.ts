ce88f6444859133de50d98865f622f216d3bca47
Juan Albarracin <jalbarracin@een.com>
2020-09-09 19:31:38 +0200

    fixed vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,19 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import "vuetify/dist/vuetify.min.css"
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+
+    theme: {
+        dark: false,
+        options: {
+            customProperties: true
+        }
+    },
+    icons: {
+        iconfont: 'mdi'
+    }
+
 });
